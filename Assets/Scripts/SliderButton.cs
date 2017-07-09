3aae9a4b9062da246b05c52729633f25441c50b7
Vinoth Pandian <vinothpandian@rwth-aachen.de>
2017-07-09 12:02:34 +0200

    Redesigned slider working with hand input

---
+++ Assets/Scripts/SliderButton.cs
@@ -0,0 +1,76 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SliderButton : MonoBehaviour, IManipulationHandler
+{
+    [SerializeField]
+    float DragSpeed = 1.5f;
+
+    [SerializeField]
+    float DragScale = 1.5f;
+
+    [SerializeField]
+    float MaxDragDistance = 3f;
+
+	[SerializeField]
+	float thrust = 10f;
+
+	private Rigidbody rb;
+
+	float distFromCam;
+        
+    Vector3 lastPosition;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody>();
+		distFromCam = Vector3.Distance (transform.position, Camera.main.transform.position);
+
+	}
+
+	[SerializeField]
+    bool draggingEnabled = true;
+    public void SetDragging(bool enabled)
+    {
+        draggingEnabled = enabled;
+    }
+
+    public void OnManipulationStarted(ManipulationEventData eventData)
+    {
+        InputManager.Instance.PushModalInputHandler(gameObject);
+        lastPosition = transform.position;
+    }
+
+    public void OnManipulationUpdated(ManipulationEventData eventData)
+    {
+        if (draggingEnabled)
+        {         
+            Drag(eventData.CumulativeDelta);
+
+            //sharing & messaging
+            //SharingMessages.Instance.SendDragging(Id, eventData.CumulativeDelta);
+        }
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData)
+    {
+        InputManager.Instance.PopModalInputHandler();
+    }
+
+    public void OnManipulationCanceled(ManipulationEventData eventData)
+    {
+        InputManager.Instance.PopModalInputHandler();
+    }
+
+    void Drag(Vector3 position)
+    {
+        var targetPosition = lastPosition + position * DragScale;
+        if (Vector3.Distance(lastPosition, targetPosition) <= MaxDragDistance)
+        {
+			rb.velocity = (targetPosition	- transform.position) * DragSpeed;
+        }
+    }
+
+}
---
+++ Assets/Scripts/TubeSliderManager.cs
@@ -0,0 +1,44 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TubeSliderManager : MonoBehaviour, IManipulationHandler
+{
+
+	public void OnManipulationStarted(ManipulationEventData eventData)
+	{
+		
+
+		if (GameObject.FindGameObjectWithTag ("SliderButton") != null) 
+		{
+			GameObject go = GameObject.FindGameObjectWithTag ("SliderButton");
+			InputManager.Instance.PushModalInputHandler(go);
+			go.GetComponent<DragButton>().DragStart(eventData.CumulativeDelta);
+		}
+
+	}
+
+	public void OnManipulationUpdated(ManipulationEventData eventData)
+	{
+		if (GameObject.FindGameObjectWithTag ("SliderButton") != null) 
+		{
+			GameObject go = GameObject.FindGameObjectWithTag ("SliderButton");
+			go.GetComponent<DragButton>().Drag(eventData.CumulativeDelta);
+		}
+
+	}
+
+	public void OnManipulationCompleted(ManipulationEventData eventData)
+	{
+		InputManager.Instance.PopModalInputHandler();
+	}
+
+	public void OnManipulationCanceled(ManipulationEventData eventData)
+	{
+		InputManager.Instance.PopModalInputHandler();
+	}
+
+
+}
